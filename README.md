11958e33c866d1471f50835fe34fe52eb7c27962
James J Balamuta <balamut2@illinois.edu>
2019-05-01 10:16:35 -0500

    Add imps wordcloud

---
+++ README.md
@@ -0,0 +1,5 @@
+# misc
+
+One-off _R_ scripts for various projects.
+
+Note: Scripts may be promoted at a later time to stand-alone projects.
---
+++ imps-2019-wordcloud/imps-abstract-word-cloud.R
@@ -0,0 +1,79 @@
+## Install Script Dependencies ----
+
+# All packages needed for script
+pkg_list = c("pdftools", "tm", "wordcloud")
+# Determine what packages are NOT installed already.
+to_install_pkgs = pkg_list[!(pkg_list %in% installed.packages()[,"Package"])]
+# Install the missing packages
+if(length(to_install_pkgs)) {
+    install.packages(to_install_pkgs, repos = "https://cloud.r-project.org")
+}
+# Load all packages
+pkg_loaded = sapply(pkg_list, require, character.only = TRUE)
+
+
+## Project Directory ----
+proj_dir = file.path("imps-2019-wordcloud")
+
+## Download the IMPS 2018 Abstract Text ----
+
+# Set target URL
+imps_url = "https://www.psychometricsociety.org/sites/default/files/IMPS%202018%20Talk%20Abstracts.pdf"
+imps_abstract_pdf_file = file.path(
+    proj_dir, tolower(gsub(" ", "-", URLdecode(basename(imps_url))))
+)
+
+# Download the PDF
+download.file(imps_url, imps_abstract_pdf_file, mode = "wb")
+
+## Construct Text Corpus ----
+
+# Parse the PDF text into an R text vector
+txt = pdf_text(imps_abstract_pdf_file)
+
+# Convert to a tm corpus
+abstract_corpus = VCorpus(VectorSource(txt))
+
+## Clean Text Corpus ----
+
+clean_abstract_corpus = function(corpus){
+    corpus = tm_map(corpus, stripWhitespace)
+    corpus = tm_map(corpus, removePunctuation)
+    corpus = tm_map(corpus, content_transformer(tolower))
+    corpus = tm_map(corpus, removeWords, stopwords("en"))
+    return(corpus)
+}
+
+abstract_corpus_cleaned = clean_abstract_corpus(abstract_corpus)
+
+## Obtain Frequencies for Word Usage ----
+
+dtm_abstract = TermDocumentMatrix(abstract_corpus_cleaned)
+
+matrix_dtm = as.matrix(dtm_abstract)
+sorted_matrix_dtm = sort(rowSums(matrix_dtm), decreasing = TRUE)
+most_popular_words = data.frame(word = names(sorted_matrix_dtm),
+                                freq = sorted_matrix_dtm)
+head(most_popular_words, 10)
+
+
+## Generate a Wordcloud based on popular words ----
+
+set.seed(55531)
+
+imps_wordcloud_export_loc =
+    paste0(tools::file_path_sans_ext(imps_abstract_pdf_file),
+           "-wordcloud.pdf")
+
+# Write to a PDF file
+pdf(imps_wordcloud_export_loc, 8, 11)
+wordcloud(
+    words = most_popular_words$word,
+    freq = most_popular_words$freq,
+    min.freq = 10,
+    max.words = 200,
+    random.order = FALSE,
+    rot.per = 0.35,
+    colors = brewer.pal(8, "Dark2")
+)
+dev.off()
